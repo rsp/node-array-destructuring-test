253f068e07408a7d81b00e42ec2aa62db6f89313
Rafał Pocztarski <r.pocztarski@gmail.com>
2017-07-13 19:19:38 +0200

    Add test

---
+++ test.js
@@ -0,0 +1,17 @@
+
+const f = (str) => {
+  const [first, second] = str.split('.');
+  // throws an undefined error
+  // (can't find a .next of undefined? -- this may not be related)
+}
+
+f('');
+f('aaa');
+f('aaa.');
+f('.aaa');
+f('.');
+f('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+f('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.aa');
+
+console.log('OK');
+
